193ca0009ca536b624280a88cf35f007ca7752aa
chuanshang <2541215569@qq.com>
2016-12-15 19:20:45 +0800

    添加个排序的测试方法

---
+++ arcgis-google&&tmap/src/main/java/cn/dayu/slmap/common/ConStant.java
@@ -0,0 +1,10 @@
+package cn.dayu.slmap.common;
+
+/**
+ * Created by chuan on 16/12/13.
+ */
+
+public class ConStant {
+    public static double locx = 120.1728577401, locy = 30.2340240947, Rzoom = 1.5E-3;
+    public static String tag = "common";
+}
